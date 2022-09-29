1e98d3d61d98ad0e162cdf62645da89b9de8aa20
melshaikh <44936830+melshaikh@users.noreply.github.com>
2022-09-29 23:21:19 +0800

    working on pomen access: 1- index.php add image, pomen type and services list. 2- services add new, select from list and delete from account list(list  based-on pomen type) 3- App APIs fixed 4- Android App test installer uploade and linked from main index.php

--- index.php
+++ index.php
@@ -399,7 +399,7 @@
   <div class="container">
     <div class="facts-wrapper">
         <div class="row">
-          <div class="col-md-3 col-sm-6 ts-facts">
+          <div class="col-md-4 col-sm-6 ts-facts">
               <div class="ts-facts-img">
                 <img loading="lazy" src="images/icon-image/fact1.png" alt="facts-img">
               </div>
@@ -409,7 +409,7 @@
               </div>
           </div><!-- Col end -->
 
-          <div class="col-md-3 col-sm-6 ts-facts mt-5 mt-sm-0">
+          <div class="col-md-4 col-sm-6 ts-facts mt-5 mt-sm-0">
               <div class="ts-facts-img">
                 <img loading="lazy" src="images/icon-image/fact2.png" alt="facts-img">
               </div>
@@ -419,23 +419,13 @@
               </div>
           </div><!-- Col end -->
 
-          <div class="col-md-3 col-sm-6 ts-facts mt-5 mt-md-0">
+          <div class="col-md-4 col-sm-6 ts-facts mt-5 mt-md-0">
               <div class="ts-facts-img">
                 <img loading="lazy" src="images/icon-image/fact3.png" alt="facts-img">
               </div>
               <div class="ts-facts-content">
                 <h2 class="ts-facts-num"><span class="counterUp" data-count="0">0</span></h2>
                 <h3 class="ts-facts-title">#Hours of Work</h3>
-              </div>
-          </div><!-- Col end -->
-
-          <div class="col-md-3 col-sm-6 ts-facts mt-5 mt-md-0">
-              <div class="ts-facts-img">
-                <img loading="lazy" src="images/icon-image/fact4.png" alt="facts-img">
-              </div>
-              <div class="ts-facts-content">
-                <h2 class="ts-facts-num"><span class="counterUp" data-count="0">0</span></h2>
-                <h3 class="ts-facts-title">#Countries Experience</h3>
               </div>
           </div><!-- Col end -->
 
